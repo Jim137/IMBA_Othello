d64081f0e0de6de08baeee3b8af9aaa9164b6a21
wen033240 <greenwenning@gapp.nthu.edu.tw>
2023-01-11 11:58:27 +0800

    correct generate y

---
+++ src/algorithm/ML_copy.py
@@ -0,0 +1,97 @@
+#generate [x]
+import sys 
+sys.path.append('../')
+from othello.util.game import game
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+import pickle
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
+from .vmap import *
+
+def generate_white_x(boards): #load a file of boards
+    output_xs = np.zeros((len(boards),64))
+    for n, board in enumerate(boards): #each board
+        x = np.array(board).flatten()
+
+        xixj = np.zeros(len(x)) #sum
+
+        for i in range(len(x)):
+            for j in range(len(x)):
+                if i == j:
+                    continue
+                else:
+                    xixj[i] += x[i]*x[j]
+        output_xs[n][:] = xixj *1
+
+    return output_xs
+
+def generate_black_x(boards): #load a file of boards
+    output_xs = np.zeros((len(boards),64))
+    for n, board in enumerate(boards): #each board
+        x = np.array(board).flatten()
+
+        xixj = np.zeros(len(x)) #sum
+
+        for i in range(len(x)):
+            for j in range(len(x)):
+                if i == j:
+                    continue
+                else:
+                    xixj[i] += x[i]*x[j]
+        output_xs[n][:] = xixj *-1
+
+    return output_xs
+
+def generate_white_y(boards, output_x, mobility=10.):
+    output_y = np.zeros(len(boards))
+    valid_move = 0  
+
+    for n, board in enumerate(boards):
+        b = game()
+        b.set_board(board)
+
+        valid_move = len(b.valid_move_white())
+        valid_move = mobility * valid_move
+        
+        for i, s in enumerate(output_x[n]):
+            output_y[n] += s * vmap_flat[i]
+
+        output_y[n] -= valid_move
+
+    return output_y 
+
+def generate_black_y(boards, output_x, mobility=10.):
+    output_y = np.zeros(len(boards))
+    valid_move = 0  
+
+    for n, board in enumerate(boards):
+        b = game()
+        b.set_board(board)
+
+        valid_move = len(b.valid_move_white())
+        valid_move = mobility * valid_move
+        
+        for i, s in enumerate(output_x[n]):
+            output_y[n] += s * vmap_flat[i]
+
+        output_y[n] += valid_move
+
+    return output_y 
+
+def KNR(x,y,model_name=str):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2 )
+
+    ktable = {'k':[], 'fscore':[], 'accuracy':[], 'precision':[], 'recall':[]}
+    
+    knr = KNeighborsRegressor(n_neighbors=4, weights="uniform", algorithm="auto", 
+        leaf_size=30, p=1, metric="minkowski", metric_params=None, n_jobs=None)
+    knr.fit(x_train,y_train)
+
+    prediction = knr.predict(x_test)
+    # print('ytest= ', y_test, 'prediction=', prediction)
+
+    pickle.dump(knr,open('model/'+model_name+'.pickle',"wb"))
+    return 
+    
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from othello.othello import *
-from algorithm.ML import *
+from algorithm.ML_copy import *
 import glob
 
 def ML_process(mobility=10.):
@@ -27,24 +27,27 @@ def ML_process(mobility=10.):
                 white_turn_boards.append(boards[i])
 
     #train white model
-    white_x = generate_x(white_turn_boards)
+    white_x = generate_white_x(white_turn_boards)
     white_y = generate_white_y(white_turn_boards, white_x, mobility)
 
     KNR(white_x,white_y,'model_white')
 
     #train black model
-    black_x = generate_x(black_turn_boards)
+    black_x = generate_black_x(black_turn_boards)
     black_y = generate_black_y(black_turn_boards, black_x, mobility)
     KNR(black_x,black_y,'model_black')
     return
 
 if __name__ == "__main__":
     # pvp()
-    # boards = load_from_txt('train_data/15x1_w.txt')
+    # boards = load_from_txt('train_data/15x3_w.txt')[0]
+    # x = generate_black_x(boards)
+    # y = generate_black_y(boards, x, 10.)
+    # print(y)
     # GreedyBot(1)
     # ML_process()
     # ML(depth=5)
-    ML_process(mobility=5.)
-    # Ising_vs_greedy(strategy=1,depth=3)
-    Ising_vs_greedy(-1,strategy=1,depth=3)
+    ML_process(mobility=10.)
+    Ising_vs_greedy(strategy=2,depth=3)
+    # Ising_vs_greedy(-1,strategy=1,depth=3)
 
